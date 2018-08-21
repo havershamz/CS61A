a4580c56ffa3307b39b16426614246b92696d336
Haversham <376305734@qq.com>
2018-08-20 21:50:31 -0700

    transfer data

--- ex.py
+++ ex.py
@@ -57,8 +57,67 @@ def gcd(m, n):
 	else:
 		return gcd(m-n, n)
 
+def left_binary_tree(tree):
+	if type(tree) != list:
+		return tree
+	if len(tree) > 2:
+		tree = [tree[:len(tree)-1], tree[len(tree)-1]]
+	return [left_binary_tree(t) for t in tree]
 
-a = [1 and 2,
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums[:] = sorted(list(set(nums)))
+        return len(nums)
+
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        length = len(prices)
+        if not prices or length == 1:
+            return 0
+        max_profit = 0
+        temp_min = prices[0]
+        j = 1
+        while j < length:
+            if prices[j] > temp_min:
+                max_profit += prices[j] - temp_min
+                temp_min = prices[j]
+                j += 1
+                continue
+            if prices[j] <= temp_min:
+                temp_min = prices[j]
+                j += 1
+        return max_profit
+
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        if k >= length:
+            k = length % k
+        elif k != 0:
+            position = length -k-1
+            count = 0
+            while count+1 < length:
+                nums[0], nums[position] = nums[position], nums[0]
+                position = (position + k) if ((position + k) < length) else (position + k - length)
+                count += 1
+
+a = Solution()
+s = [1,2]
+a.rotate(s, 2)
+print(s)
+
+"""a = [1 and 2,
 2 and 1,
 1 and 0,
 0 and 1,
@@ -67,5 +126,4 @@ a = [1 and 2,
 1 or 0,
 0 or 1,
 None or None]
-print(a)
-
+print(a)"""
